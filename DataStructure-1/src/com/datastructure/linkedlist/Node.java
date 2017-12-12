6be6fde9b8f3f3da7d083561226bb7c4b3a4f302
shaikhwa <shaikhwa@MUMWD101061.ASIAPAC.NOM>
2017-12-12 14:52:48 +0530

    Linked List Data Structure

---
+++ DataStructure-1/src/com/datastructure/linkedlist/Node.java
@@ -0,0 +1,21 @@
+package com.datastructure.linkedlist;
+
+public class Node {
+
+	public int value;
+	public Node node;
+	
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public Node getNode() {
+		return node;
+	}
+	public void setNode(Node node) {
+		this.node = node;
+	}
+	
+}
